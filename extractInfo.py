a93b21aeea7f6975044b834af7adf3bd6e4c63fc
aforakhilesh <aforakhileshgit@gmail.com>
2023-05-09 15:32:40 +0530

    Added condition for extraction under keywords

--- extractInfo.py
+++ extractInfo.py
@@ -38,7 +38,7 @@ def main():
     df = pd.read_csv("links.csv")
 
     # Change the number of files by changing the value in range(), keeping len(df) runs for all links, range(n) runs for "n" links.
-    for i in range(len(df)):
+    for i in range(0,5):
         
         # change "te" to other language tags if needed, (like "hi","ur","bn")..
         try:
@@ -49,23 +49,28 @@ def main():
         # Can also update the output path if needed.
         string  = "./%s.txt" % i
         WordCount += len(t.split(" "))
-        t = sent_tokenize(t)
-        SentenceCount += len(t)
-
-        try:
-            with open(string, "w") as f:
-                f.write(df["titles"][i])
-                f.write("\n")
-                f.write("\n")
-                f.write(df["Urls"][i])
-                f.write("\n")
-                f.write("\n")
-                for line in t:
-                    f.write(line)
-                    f.write("\n")
-                f.close()
-        except:
-            pass
+        t1 = sent_tokenize(t)
+        SentenceCount += len(t1)
+        
+        # Check for keywords in the extracted text.
+        keywords = ["స్వాతంత్ర్యం","పాకిస్తాన్","హిందూస్తాన్","1947"]
+        words = t.split(" ")
+        for word in words:
+            if word in keywords:
+                try:
+                    with open(string, "w") as f:
+                        f.write(df["titles"][i])
+                        f.write("\n")
+                        f.write("\n")
+                        f.write(df["Urls"][i])
+                        f.write("\n")
+                        f.write("\n")
+                        for line in t:
+                            f.write(line)
+                            f.write("\n")
+                        f.close()
+                except:
+                    pass
 
 
 if __name__ == '__main__':
